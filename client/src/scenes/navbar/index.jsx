7c9aaddc97aaaf788cfd3f9e052633fd92a04d72
Roger Wells <rsw359@gmail.com>
2023-03-22 18:48:35 +0900

    theme palatte and styled component setup, Navabar init setup

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -1,4 +1,39 @@
+import { useState } from "react";
+import {
+  Box,
+  IconButton,
+  InputBase,
+  Typography,
+  Select,
+  MenuItem,
+  FormControl,
+  useTheme,
+  useMediaQuery,
+} from "@mui/material";
+
+import {
+  Search,
+  Message,
+  DarkMode,
+  LightMode,
+  Notifications,
+  Help,
+  Menu,
+  Close,
+} from "@mui/icons-material";
+import { useDispatch, useSelector } from "react-redux";
+import { setMode, setLogout } from "state";
+import { useNavigate } from "react-router-dom";
+import FlexBetween from "components/FlexBetween";
+
 const NavBar = () => {
+  const [isMobileMenuToggled, setIsMobileMenuToggled] = useState(false);
+  const dispatch = useDispatch(); //used to dispatch actions from the reducers
+  const navigate = useNavigate(); //used to navigate to different routes
+  const user = useSelector((state) => state.user); //used to get the user from the redux store
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)"); //used to check if the screen is mobile or not\
+  const theme = useTheme(); //used to get the theme from theme.js
+
   return <div>Navbar</div>;
 };
 
