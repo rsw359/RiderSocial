6e94aee06bbd2922f03992adda2ba3b9e598a821
Roger Wells <rsw359@gmail.com>
2023-03-29 22:11:26 +0900

    login, register, and auth completed

--- client/src/App.js
+++ client/src/App.js
@@ -9,22 +9,29 @@ import { createTheme } from "@mui/material/styles";
 import { themeSettings } from "./theme";
 
 function App() {
-  const mode = useSelector((state) => state.mode);
-  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
-  return (
-    <div className="app">
-      <BrowserRouter>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <Routes>
-            <Route path="/" element={<LoginPage />} />
-            <Route path="/home" element={<HomePage />} />
-            <Route path="/profile/:userId" element={<ProfilePage />} />
-          </Routes>
-        </ThemeProvider>
-      </BrowserRouter>
-    </div>
-  );
+	const mode = useSelector((state) => state.mode);
+	const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+	const isAuth = Boolean(useSelector((state) => state.token));
+	return (
+		<div className="app">
+			<BrowserRouter>
+				<ThemeProvider theme={theme}>
+					<CssBaseline />
+					<Routes>
+						<Route path="/" element={<LoginPage />} />
+						<Route
+							path="/home"
+							element={isAuth ? <HomePage /> : <Navigate to="/" />}
+						/>
+						<Route
+							path="/profile/:userId"
+							element={isAuth ? <ProfilePage /> : <Navigate to="/" />}
+						/>
+					</Routes>
+				</ThemeProvider>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
