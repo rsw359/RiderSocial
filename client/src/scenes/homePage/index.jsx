dba6c80394a23441af5e6f4dbe181218cffcb4f7
Roger Wells <rsw359@gmail.com>
2023-04-05 14:23:32 +0900

    friends list created

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -5,6 +5,7 @@ import UserWidget from "scenes/widgets/UserWidget";
 import MyPostWidget from "scenes/widgets/MyPostWidget";
 import PostsWidget from "scenes/widgets/PostsWidget";
 import AdWidget from "scenes/widgets/AdWidget";
+import FriendsList from "scenes/widgets/FriendsList";
 
 const HomePage = () => {
 	const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
@@ -34,6 +35,7 @@ const HomePage = () => {
 					<Box flexBasis="26%">
 						<AdWidget />
 						<Box m="2rem 0" />
+						<FriendsList userId={_id} />
 					</Box>
 				)}
 			</Box>
