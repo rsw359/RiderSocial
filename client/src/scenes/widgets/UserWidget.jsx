efea1fee53f388d479b2f9e9c63bd68beb5e01ef
Roger Wells <rsw359@gmail.com>
2023-09-11 11:50:34 +0900

    preparing for strava integration

--- client/src/scenes/widgets/UserWidget.jsx
+++ client/src/scenes/widgets/UserWidget.jsx
@@ -3,7 +3,9 @@ import {
 	EditOutlined,
 	LocationOnOutlined,
 	WorkOutlineOutlined,
+	PedalBikeOutlined,
 } from "@mui/icons-material";
+
 import { Box, Typography, Divider, useTheme } from "@mui/material";
 import UserImage from "components/UserImage";
 import FlexBetween from "components/FlexBetween";
@@ -86,65 +88,37 @@ const UserWidget = ({ userId, picturePath }) => {
 					<LocationOnOutlined fontSize="large" sx={{ color: main }} />
 					<Typography color={medium}>{location}</Typography>
 				</Box>
-				<Box display="flex" alignItems="center" gap="1rem">
+				<Box display="flex" alignItems="center" gap="1rem" mb="0.5rem">
 					<WorkOutlineOutlined fontSize="large" sx={{ color: main }} />
 					<Typography color={medium}>{occupation}</Typography>
 				</Box>
+				<Divider />
+				<Box display="flex" alignItems="center" gap="1rem" mt="1rem">
+					<PedalBikeOutlined fontSize="large" sx={{ color: main }} />
+					<Typography color={medium}>{occupation}</Typography>
+				</Box>
 			</Box>
-
 			<Divider />
 
 			{/* THIRD ROW */}
-			<Box p="1rem 0">
-				<FlexBetween mb="0.5rem">
-					<Typography color={medium}>Who's viewed your profile</Typography>
-					<Typography color={main} fontWeight="500">
-						{viewedProfile}
-					</Typography>
-				</FlexBetween>
-				<FlexBetween>
-					<Typography color={medium}>Impressions of your post</Typography>
-					<Typography color={main} fontWeight="500">
-						{impressions}
-					</Typography>
-				</FlexBetween>
-			</Box>
-
-			<Divider />
 
 			{/* FOURTH ROW */}
 			<Box p="1rem 0">
 				<Typography fontSize="1rem" color={main} fontWeight="500" mb="1rem">
-					Social Profiles
+					Strava Account
 				</Typography>
 
 				<FlexBetween gap="1rem" mb="0.5rem">
 					<FlexBetween gap="1rem">
 						<img
-							src="https://res.cloudinary.com/dvdtehk9y/image/upload/v1693792088/twitter_cu9tji.png"
-							alt="twitter"
+							src="https://res.cloudinary.com/dvdtehk9y/image/upload/c_scale,e_sharpen:1000,w_25/v1694393861/strava_plfvkk.png"
+							alt="strava logo"
 						/>
 						<Box>
 							<Typography color={main} fontWeight="500">
-								Twitter
+								Strava
 							</Typography>
-							<Typography color={medium}>Social Network</Typography>
-						</Box>
-					</FlexBetween>
-					<EditOutlined sx={{ color: main }} />
-				</FlexBetween>
-
-				<FlexBetween gap="1rem">
-					<FlexBetween gap="1rem">
-						<img
-							src="https://res.cloudinary.com/dvdtehk9y/image/upload/v1693792131/linkedin_c7k8jj.png"
-							alt="linkedin"
-						/>
-						<Box>
-							<Typography color={main} fontWeight="500">
-								Linkedin
-							</Typography>
-							<Typography color={medium}>Network Platform</Typography>
+							<Typography color={medium}>Link</Typography>
 						</Box>
 					</FlexBetween>
 					<EditOutlined sx={{ color: main }} />
