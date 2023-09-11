8bd535cf35ae24d8fd339011db7b96c7c5ec871a
Roger Wells <rsw359@gmail.com>
2023-09-11 15:07:01 +0900

    changed all server urls

--- client/src/scenes/widgets/UserWidget.jsx
+++ client/src/scenes/widgets/UserWidget.jsx
@@ -24,10 +24,13 @@ const UserWidget = ({ userId, picturePath }) => {
 	const main = palette.neutral.main;
 
 	const getUser = async () => {
-		const response = await fetch(`https://44.226.145.213/users/${userId}`, {
-			method: "GET",
-			headers: { Authorization: `Bearer ${token}` },
-		});
+		const response = await fetch(
+			`https://romance-server.onrender.com/users/${userId}`,
+			{
+				method: "GET",
+				headers: { Authorization: `Bearer ${token}` },
+			}
+		);
 		const data = await response.json();
 		setUser(data);
 	};
