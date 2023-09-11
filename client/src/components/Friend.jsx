6feaaaecc12e08bf2947e99bc1f83024296fb41d
Roger Wells <rsw359@gmail.com>
2023-09-11 13:52:40 +0900

    prelim deploymnt

--- client/src/components/Friend.jsx
+++ client/src/components/Friend.jsx
@@ -24,7 +24,7 @@ const Friend = ({ friendId, name, subtitle, userPicturePath, isProfile }) => {
 
 	const patchFriend = async () => {
 		const response = await fetch(
-			`http://localhost:3001/users/${_id}/${friendId}`,
+			`http://44.226.145.213/users/${_id}/${friendId}`,
 			{
 				method: "PATCH",
 				headers: {
