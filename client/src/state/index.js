d6852fecde9edb069a217fa4903b310676c6d823
Roger Wells <rsw359@gmail.com>
2023-09-12 10:20:49 +0900

    fixed null error on post delete by adding conditional to state store

--- client/src/state/index.js
+++ client/src/state/index.js
@@ -1,47 +1,54 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  mode: "light",
-  user: null,
-  token: null,
-  posts: [],
+	mode: "light",
+	user: null,
+	token: null,
+	posts: [],
 };
 
 export const authSlice = createSlice({
-  name: "auth",
-  initialState,
-  reducers: {
-    setMode: (state) => {
-      state.mode = state.mode === "light" ? "dark" : "light";
-    },
-    setLogin: (state, action) => {
-      state.user = action.payload.user;
-      state.token = action.payload.token;
-    },
-    setLogout: (state) => {
-      state.user = null;
-      state.token = null;
-    },
-    setFriends: (state, action) => {
-      if (state.user) {
-        state.user.friends = action.payload.friends;
-      } else {
-        console.error("user friends non existant ");
-      }
-    },
-    setPosts: (state, action) => {
-      state.posts = action.payload.posts;
-    },
-    setPost: (state, action) => {
-      const updatedPosts = state.posts.map((post) => {
-        if (post._id === action.payload.post._id) return action.payload.post;
-        return post;
-      });
-      state.posts = updatedPosts;
-    },
-  },
+	name: "auth",
+	initialState,
+	reducers: {
+		setMode: (state) => {
+			state.mode = state.mode === "light" ? "dark" : "light";
+		},
+		setLogin: (state, action) => {
+			state.user = action.payload.user;
+			state.token = action.payload.token;
+		},
+		setLogout: (state) => {
+			state.user = null;
+			state.token = null;
+		},
+		setFriends: (state, action) => {
+			if (state.user) {
+				state.user.friends = action.payload.friends;
+			} else {
+				console.error("user friends non existant ");
+			}
+		},
+		setPosts: (state, action) => {
+			state.posts = action.payload.posts;
+		},
+		setPost: (state, action) => {
+			//action.payload.post is null when deleting a post, so we need this conditional
+			if (action.payload.post === null) {
+				state.posts = state.posts.filter(
+					(post) => post._id !== action.payload.postId
+				);
+			} else {
+				const updatedPosts = state.posts.map((post) => {
+					if (post._id === action.payload.post._id) return action.payload.post;
+					return post;
+				});
+				state.posts = updatedPosts;
+			}
+		},
+	},
 });
 
 export const { setMode, setLogin, setLogout, setFriends, setPosts, setPost } =
-  authSlice.actions;
+	authSlice.actions;
 export default authSlice.reducer;
